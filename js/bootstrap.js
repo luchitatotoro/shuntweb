bb79a69622de5301b065d3fdc1fb2e7942dfb84b
Luchita Totoro <luciaparral@gmail.com>
2015-03-08 22:17:01 -0300

    show table

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -1962,7 +1962,12 @@ $( "#imageShows" ).click(function() {
   $("html, body").animate({ scrollTop: $('#shows').offset().top }, 1000);
 });
 
+$('.Date').click(function(){
+   $(this).find('span').text(function(_, value){return value=='-'?'+':'-'});
+    $(this).nextUntil('tr.Date').slideToggle(100, function(){
+    });
+});
 
-$('#showsDates').jExpand();
+
 
 }(jQuery);
